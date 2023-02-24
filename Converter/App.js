ba44d420868e632ac498f423438aadaab3da7383
Hyerin <hyerin.bcit@gmail.com>
2023-02-23 16:20:09 -0800

    Added select

--- Converter/App.js
+++ Converter/App.js
@@ -1,10 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { SelectList } from "react-native-dropdown-select-list";
 
 export default function App() {
+  // const [counter, setCounter] = useState(0);
+  const [selected, setSelected] = useState();
+
+  const data = [
+    { key: "1", value: "Min to Hrs" },
+    { key: "2", value: "Kg to Lb" },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Super Convert</Text>
+      <SelectList
+        setSelected={(val) => setSelected(val)}
+        data={data}
+        save="value"
+        search={false}
+        maxHeight={80}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +29,14 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    top: 200,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 10,
   },
 });
